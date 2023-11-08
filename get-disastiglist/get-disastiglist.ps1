7294d1541078bab0936e5f6855bd9598048c233c
Brad <bradleynielsen@gmail.com>
2023-11-07 20:24:40 -0600

    adding scraper with dl option

--- get-disastiglist/get-disastiglist.ps1
+++ get-disastiglist/get-disastiglist.ps1
@@ -1,75 +1,43 @@
-﻿
-#"DataTables_Table_0_length"
-
-
-
-
+﻿#"DataTables_Table_0_length"
 
 cls
 $scriptRootPath =  $PSScriptRoot
 
-
-$uri = "https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/U_Adobe_Acrobat_Pro_DC_Continuous_V2R1_STIG.zip"
-$dlPath = $scriptRootPath+"\download\U_Adobe_Acrobat_Pro_DC_Continuous_V2R1_STIG.zip"
-#$dlPath = $env:HOMEPATH+"\tempsfdsadf.bin"
-#(New-Object System.Net.WebClient).DownloadFile($uri, $dlPath)
-
-
-Invoke-WebRequest -URI $uri -OutFile $dlPath
-
-
-
-
 $webAddress =  'https://public.cyber.mil/stigs/downloads'
 $webPage = Invoke-WebRequest -Uri $webAddress 
-
-#$fileDiv = $webPage.ParsedHtml.getElementsByClassName("file") 
-#$innerHTML = $tableDiv[0].innerHTML | ConvertTo-Html
-
-$downloadTable = $webPage.ParsedHtml.getElementsByClassName("downloadTable") 
-$downloadTable = $webPage.ParsedHtml.getElementsByTagName("file") 
 
 $trDivs = $webPage.ParsedHtml.IHTMLDocument3_getElementsByTagName("tr")
 
 
 $results = @()
 
-
-function convertForDate($innerText){
-    try{
-        $date = $innerText
-        #$date.Replace(" ","-")
-        $value = [datetime]::parseexact($date.trim(), 'yyyy MM dd', $null)
-    }catch{
-        $value = $innerText
-    }
-    return $object
-}
-
-
-
 foreach ($tr in $trDivs){
     
     $trChildren = $tr.children
 
     foreach ($td in $trChildren){
+
         $tdChildren = $td.children
+
         $lineObject = [PSCustomObject]@{
             title = '-'
             date  = '-'
+            link  = '-'
         }
         
-        
-        #set title
+        #set title and download link
         if ($td.className -eq "title_column" ){
             $keyName = $td.className
             foreach($tdChild in $tdChildren){
+                
                 if ($tdChild.tagName -eq "span" ){
                      $titleValue = $tdChild.innerText
                 }
+                if ($tdChild.tagName -eq "A" ){
+                     $linkValue = $tdChild.href
+                }
             }
         }
-
 
         #set date
         if ($td.className -eq "updated_column" ){
@@ -81,14 +49,32 @@ foreach ($tr in $trDivs){
                 }
             }
         }
-
-
-
     }
+
     $lineObject.title = $titleValue 
     $lineObject.date  = $dateValue
-    $lineObject
+    $lineObject.link  = $linkValue
+    #$lineObject
     $results +=     $lineObject
+
+    $uri = $lineObject.link
+
+    if ($uri){
+        $pathIndex = (($uri.Split("/")).count) - 1
+        $nameArray = $uri.Split("/")
+        $filename  = $nameArray[$pathIndex]
+
+        $dlPath = $scriptRootPath+"\download\"+$filename
+
+        "downloading     $titleValue "
+        Invoke-WebRequest -URI $uri -OutFile $dlPath
+        Start-Sleep -Seconds 15  
+          
+    }
+
 }
 
 $results | Sort-Object -Property date -Descending  | export-csv -Path   "$scriptRootPath\disaStigList.csv" -NoTypeInformation
+
+    #$dlPath = $env:HOMEPATH+"\tempsfdsadf.bin"
+    #(New-Object System.Net.WebClient).DownloadFile($uri, $dlPath)
